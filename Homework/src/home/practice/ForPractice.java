1abde32c02513b4123c29bddb21a81c4566b7030
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-02-04 01:38:12 +0900

    home

---
+++ Homework/src/home/practice/ForPractice.java
@@ -0,0 +1,17 @@
+package home.practice;
+
+public class ForPractice {
+
+	public static void main(String[] args) {
+		// for문 연습하기
+		
+		// 안녕하세요1,안녕하세요2,... 10까지 차례로 출력
+		
+		String hello = "안녕하세요";
+		
+		for (int i = 1; i <= 10 ; i++) {
+			System.out.println(hello + i);
+		}
+	}
+
+}
