d1ef08549ac0eb7e5723f3c9f76d7b9fe348e9ac
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-02-05 17:48:03 +0900

    백준

--- Homework/src/baekjoon/Main.java
+++ Homework/src/baekjoon/Main.java
@@ -6,11 +6,15 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
 		
-		long A = sc.nextLong();
-		long B = sc.nextLong();
-		long C = sc.nextLong();
-		
-		System.out.println(A + B + C);
+		if (A>B) {
+			System.out.println(">");
+		} else if (A<B) {
+			System.out.println("<");
+		} else if (A == B) {
+			System.out.println("==");
+		}
 	}
 }
