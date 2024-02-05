97fd345f994f78147f258c745a08ac819237fdf4
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-02-06 02:31:51 +0900

    home

--- Homework/src/baekjoon/Main.java
+++ Homework/src/baekjoon/Main.java
@@ -6,15 +6,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
 		
-		if (A>B) {
-			System.out.println(">");
-		} else if (A<B) {
-			System.out.println("<");
-		} else if (A == B) {
-			System.out.println("==");
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if (H < 0 && (M - 45) < 0) {
+			System.out.printf("%d %d",23, 60  + (M - 45));
+		} else if (H > 0 && (M - 45) < 0) {
+			System.out.printf("%d %d", H-1, 60  + (M - 45));
+		} else {
+			System.out.printf("%d %d", H-1, M-1);
 		}
 	}
 }
