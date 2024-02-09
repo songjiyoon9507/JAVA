936bd97ca9d4f0cef19e376b5908e553b7970fc8
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-02-10 01:37:46 +0900

    oopPractice

--- Homework/src/baekjoon/Main.java
+++ Homework/src/baekjoon/Main.java
@@ -1,10 +1,29 @@
 package baekjoon;
 
-public class Main {
-	public static void main(String[] args) {
-		
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		String str;
+		
+		while((str=br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+			// 공백으로 분리 ," " 안 넣으면 출력 안됨
+			
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			sb.append(A+B).append("\n");
+		}
+		System.out.print(sb);
 		
 	}
 }
---
+++ Homework/src/baekjoon/Practice.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Practice {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A+B);
+		}
+	}
+}
