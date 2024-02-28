09cf5e13f582aff353164166e118b54264764e9c
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-02-28 17:48:11 +0900

    백준

--- Homework/src/baekjoon/Main.java
+++ Homework/src/baekjoon/Main.java
@@ -1,57 +1,31 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String str = br.readLine();
 		
-		int result = 0;
+		StringBuilder sb = new StringBuilder();
 		
-		for(int i = 0 ; i < str.length() ; i++) {
-			
-			switch(str.charAt(i)) {
-			
-			case 'A' : case 'B' : case 'C' :
-				result += 3;
-				break;
-			
-			case 'D' : case 'E' : case 'F' :
-				result += 4;
-				break;
-				
-			case 'G' : case 'H' : case 'I' :
-				result += 5;
-				break;
-				
-			case 'J' : case 'K' : case 'L' :
-				result += 6;
-				break;
-				
-			case 'M' : case 'N' : case 'O' :
-				result += 7;
-				break;
-				
-			case 'P' : case 'Q' : case 'R' : case 'S' :
-				result += 8;
-				break;
-				
-			case 'T' : case 'U' : case 'V' :
-				result += 9;
-				break;
-				
-			case 'W' : case 'X' : case 'Y' : case 'Z' :
-				 result += 10;
-				 break;
-				 
-			default : break;
-			}
+		sb.append(str);
+		
+		String temp = sb.toString();
+		
+		String[] arr =temp.split(" ");
+		
+		for(String s : arr) {
+			System.out.println(s);
 		}
-		System.out.println(result);
+		
+		
 	}
 }
