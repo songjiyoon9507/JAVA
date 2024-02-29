274fe470e1440480ec76bb630f86f4cfd5512274
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-02-29 17:42:56 +0900

    알고리즘

--- Homework/src/baekjoon/Main.java
+++ Homework/src/baekjoon/Main.java
@@ -1,8 +1,18 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		String str = br.readLine();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		// for문 돌면서 str split 해서 넣고 개수 빼기
 	}
 }
