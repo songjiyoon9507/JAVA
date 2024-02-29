60b61d77bda7c58a97186c75873df6aaf4648e0f
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-02-29 17:37:12 +0900

    백준

--- Homework/src/baekjoon/Main.java
+++ Homework/src/baekjoon/Main.java
@@ -1,30 +1,7 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String str = br.readLine();
-		
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append(str);
-		
-		String temp = sb.toString();
-		
-		String[] arr =temp.split(" ");
-		
-		for(String s : arr) {
-			System.out.println(s);
-		}
+	public static void main(String[] args) {
 		
 		
 	}
