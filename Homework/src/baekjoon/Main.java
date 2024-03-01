a0d21512eeff993caea140bb47359e0038b4f4a7
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-03-02 00:07:23 +0900

    java

--- Homework/src/baekjoon/Main.java
+++ Homework/src/baekjoon/Main.java
@@ -1,18 +1,42 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String str = br.readLine();
+		int king = 1;
+		int queen = 1;
+		int rook = 2;
+		int bishop = 2;
+		int knight = 2;
+		int pawn = 8;
 		
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		king = king - Integer.parseInt(st.nextToken());
+		queen = queen - Integer.parseInt(st.nextToken());
+		rook = rook - Integer.parseInt(st.nextToken());
+		bishop = bishop - Integer.parseInt(st.nextToken());
+		knight = knight - Integer.parseInt(st.nextToken());
+		pawn = pawn - Integer.parseInt(st.nextToken());
 		
-		// for문 돌면서 str split 해서 넣고 개수 빼기
+		bw.write(king + " ");
+		bw.write(queen + " ");
+		bw.write(rook + " ");
+		bw.write(bishop + " ");
+		bw.write(knight + " ");
+		bw.write(pawn + " ");
+		
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
