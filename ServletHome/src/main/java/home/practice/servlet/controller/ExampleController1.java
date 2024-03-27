e6e6ce282f8274ac3f42e3c9be28f3470ef6e616
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-03-28 00:10:19 +0900

    servlet복습

---
+++ ServletHome/src/main/java/home/practice/servlet/controller/ExampleController1.java
@@ -0,0 +1,7 @@
+package home.practice.servlet.controller;
+
+// Controller : 요청(Request)에 따라 어떤 Service 를 호출할지 "제어"하고,
+//              어떻게 응답(Response)을 할지 "제어"하는 역할
+public class ExampleController1 {
+	
+}
