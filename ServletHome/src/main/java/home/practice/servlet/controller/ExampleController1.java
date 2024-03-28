42aa01100d8ed73d267c3da5002732af87ac5a0f
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-03-28 17:47:56 +0900

    복습

--- ServletHome/src/main/java/home/practice/servlet/controller/ExampleController1.java
+++ ServletHome/src/main/java/home/practice/servlet/controller/ExampleController1.java
@@ -1,7 +1,15 @@
 package home.practice.servlet.controller;
 
+import jakarta.servlet.http.HttpServlet;
+
 // Controller : 요청(Request)에 따라 어떤 Service 를 호출할지 "제어"하고,
 //              어떻게 응답(Response)을 할지 "제어"하는 역할
-public class ExampleController1 {
+public class ExampleController1 extends HttpServlet{
+	// 클라이언트 -(요청)-> 서버(Controller -> Service -> DAO -> DB)
+	// DB에서 맞는 값을 들고 돌아옴
 	
+	// Java 코드로 웹페이지를 만들 수 있게 해주는 기술
+	// Servlet
+	// HttpServlet 을 상속 받아서 쓸 수 있는 메서드들
+	// Ctrl + SpaceBar
 }
