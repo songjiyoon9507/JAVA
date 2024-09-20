feaa4d7679e42fb495bd1396efc1f723d4105791
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-09-20 10:23:37 +0900

    백준 그룹 단어 체커

--- Homework/src/baekjoon/practice/GroupWord.java
+++ Homework/src/baekjoon/practice/GroupWord.java
@@ -1,10 +1,10 @@
-package baekjoon;
+package baekjoon.practice;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class GroupWord {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
